8d7e3e1c59fcf6357210636060e1ac2c3f816e7b
Zeane Capuy <zeane_denel_capuy@dlsl.edu.ph>
2026-08-12 00:09:16 +0800

    fix: comments in server.js

--- server.js
+++ server.js
@@ -5,27 +5,24 @@ import router from "./src/routes/index.js";
 
 const app = express();
 
+// Middleware
 app.use(cors());
 app.use(morgan("dev"));
 app.use(express.json());
 
+// Routes
 app.use("/api", router);
 
+// Central Error Handling Middleware (must be LAST app.use)
 app.use((err, req, res, next) => {
   console.error(err.message);
   const status = err.status || 500;
   res.status(status).json({ error: err.message });
 });
 
+// Server Initialization
 const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, () => {
   console.log(`Server running on http://localhost:${PORT}`);
 });
-
-// test and visit:
-// localhost:3000/api/tasks
-// localhost:3000/api/tasks/1
-// localhost:3000/api/users
-
-// working
